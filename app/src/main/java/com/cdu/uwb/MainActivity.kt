407e7e9fe54e6ae67adc04e17136b347df808aa6
WT <2901720137@qq.com>
2021-11-08 12:19:33 +0800

    添加了主界面布局，和侧滑栏及选项

--- app/src/main/java/com/cdu/uwb/MainActivity.kt
+++ app/src/main/java/com/cdu/uwb/MainActivity.kt
@@ -3,41 +3,58 @@ package com.cdu.uwb
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.cdu.uwb.activity.MapRouteVewActivity
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener{
 
     companion object{
-        @JvmStatic
-        val TAG = "MainActivity"
+        const val TAG = "MainActivity"
     }
-
-    private lateinit var start_maprouteview: Button
-    private lateinit var start_gyroscope: Button
+    private lateinit var toolbar: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
-        start_maprouteview = findViewById(R.id.start_maprouteview)
-        start_gyroscope = findViewById(R.id.start_gyroscope)
-        start_maprouteview.setOnClickListener(this)
-        start_gyroscope.setOnClickListener(this)
+
+        toolbar = findViewById(R.id.toolbar)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navView = findViewById(R.id.navView)
+        setSupportActionBar(toolbar)
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
+        //默认选中这个界面
+        navView.setCheckedItem(R.id.home_page)
+        //点击一个就自动关闭
+        navView.setNavigationItemSelectedListener {
+            drawerLayout.closeDrawers()
+            true
+        }
     }
 
     override fun onClick(v: View?) {
         when(v?.id){
-            R.id.start_maprouteview -> {
-                Toast.makeText(this, "You clicked start_maprouteview.", Toast.LENGTH_SHORT).show()
-                var intent = Intent(this, MapRouteVewActivity::class.java)
-                startActivity(intent)
-            }
-            R.id.start_gyroscope -> {
-                Toast.makeText(this, "You clicked start_gyroscope.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
             }
         }
+        return true
     }
 }
--- app/src/main/java/com/cdu/uwb/activity/MapRouteVewActivity.kt
+++ app/src/main/java/com/cdu/uwb/activity/MapRouteVewActivity.kt
@@ -1,39 +1,48 @@
 package com.cdu.uwb.activity
 
+import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
+import android.animation.ObjectAnimator.ofFloat
+import android.animation.ValueAnimator
+import android.content.Context
 import android.graphics.Path
 import android.os.Build
 import android.os.Bundle
+import android.renderscript.Sampler
+import android.util.Log
 import android.view.View
+import android.view.WindowManager
 import android.view.animation.TranslateAnimation
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.cdu.uwb.R
 import com.cdu.uwb.ui.MapRouteView
 
 class MapRouteVewActivity: AppCompatActivity() {
 
-    private lateinit var mNavigationArrow: ImageView
     private lateinit var mMapRouteView: MapRouteView
+    private lateinit var mArrow: ImageView
+    private lateinit var mMapRoutelayout: RelativeLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.map_route_layout)
         supportActionBar?.hide()
+        MovingArrow()
     }
 
-    //让用户位置的箭头图片随路线移动
-    private fun MoveNavigationArrow() {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
-            var path = Path().apply {
-                moveTo(mMapRouteView.mPositionPoint[0] - 50f, mMapRouteView.mPositionPoint[1] - 50f)
-                lineTo(mMapRouteView.mPositionPoint[0] - 50f, mMapRouteView.mPositionPoint[1] - 50f)
-            }
-            val animator = ObjectAnimator.ofFloat(mNavigationArrow, View.X,
-                View.Y, path).apply {
-                duration = 1000
-                start()
-            }
+    private fun MovingArrow() {
+        //点击屏幕就可以让图片从当前位置移动到目标位置
+        mArrow = findViewById(R.id.navigation_arrow)
+        mMapRouteView = findViewById(R.id.maprouteview)
+        mMapRoutelayout = findViewById(R.id.maproutelayout)
+
+        //有一种思想，使用valueanimator让imageview从当前位置移动到对应的位置，使用imageview.setX和imageview.setY方法，
+        // 只要足够频繁就能形成动画效果，objectanimator也是valueanimator形成的
+        mMapRoutelayout.setOnClickListener {
+
         }
     }
+
 }
